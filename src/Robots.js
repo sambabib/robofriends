a5a80c690c26f974c23f360ecff3b29f7cf0d244
sambabib <36554009+sambabib@users.noreply.github.com>
2020-07-21 03:58:13 +0100

    Add files via upload

---
+++ src/Robots.js
@@ -0,0 +1,72 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Paul George",
+        username: "PG13",
+        email: "peegee@april.biz"
+    },
+
+    {
+        id: 2,
+        name: "Denis Schroder",
+        username: "Denis",
+        email: "dennis@january.biz"
+    },
+
+    {
+        id: 3,
+        name: "LeBron James",
+        username: "Bron",
+        email: "bron@february.biz"
+    },
+
+    {
+        id: 4,
+        name: "Damian Lillard",
+        username: "Dame",
+        email: "dame@march.biz"
+    },
+
+    {
+        id: 5,
+        name: "Russell Westbrook",
+        username: "Brodie",
+        email: "bigruss@may.biz"
+    },
+
+    {
+        id: 6,
+        name: "Alex Caruso",
+        username: "CaruGoat",
+        email: "caruso@june.biz"
+    },
+
+    {
+        id: 7,
+        name: "Kawhi Leonard",
+        username: "Claw",
+        email: "claw@july.biz"
+    },
+
+    {
+        id: 8,
+        name: "Stephen Curry",
+        username: "Steph",
+        email: "steph@august.biz"
+    },
+
+    {
+        id: 9,
+        name: "James Harden",
+        username: "Beard",
+        email: "beard@september.biz"
+    },
+
+    {
+        id: 10,
+        name: "Joel Embiid",
+        username: "Jojo",
+        email: "jojo@october.biz"
+    },
+
+]
